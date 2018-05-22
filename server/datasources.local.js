6f7bfad5be2acf5eb963bdb9d5af7eabf94c2425
Samuel Kim <caelum.modus@gmail.com>
2018-05-21 19:57:23 -0700

    updated loopback datasources

---
+++ server/datasources.local.js
@@ -0,0 +1,17 @@
+module.exports = {
+    "db": {
+      "name": "db",
+      "connector": "memory"
+    },
+    "MongoDB": {
+      "host": "0.0.0.0",
+      "port": 27017,
+      "url": "",
+      "database": "",
+      "password": "",
+      "name": "MongoDB",
+      "user": "",
+      "connector": "mongodb"
+    }
+  }
+  
